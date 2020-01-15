032ed5263d8ca1c55c05c21051266556ffbc52a3
gremarsl <marcel.greubel@gmail.com>
2020-01-15 16:34:06 +0100

    add constraints with two circles

--- main.py
+++ main.py
@@ -5,42 +5,62 @@ import random
 from sympy import *
 import numpy as np
 
-def overlay_constraint_rectangle_circle(rectangle,circle):
 
-    def createLine(tuplearray):
-        delta_x = tuplearray[1][0] - tuplearray[0][0]
-        delta_y = tuplearray[1][1] - tuplearray[0][1]
+def overlay_constraint_rectangle_circle(rectangle, circle):
+    def createLine(t1, t2):
+        delta_x = t2[0] - t1[0]
+        delta_y = t2[1] - t1[1]
         m = delta_y / delta_x
-        c = tuplearray[0][1] - m * tuplearray[0][0]
+        c = t1[1] - m * t1[0]
         return m, c
 
-    m,c=createLine([(rectangle.get_x,rectangle.get_y),(rectangle.get_x+rectangle.get_width,rectangle.get_y)])
-    x,xm, y,ym, r,m,c = symbols('x,xm,y,ym,r,m,c')
+    # TODO for every edge and Strecke
+    m, c = createLine((rectangle.get_x(), rectangle.get_y()),
+                      (rectangle.get_x() + rectangle.get_width(), rectangle.get_y()))
+    r = circle.get_radius()
+    xm = circle.get_center()[0]
+    ym = circle.get_center()[1]
 
-    x12=solve((x-xm)**2+(m*x+c-ym)**2-r**2,x)
-    return x12
+    # solution for simplity:
+    Discriminant = (-2 * xm + 2 * m * c - 2 * m * ym) ** 2 - 4 * (1 + m ** 2) * (
+                xm ** 2 + c ** 2 + ym ** 2 - r ** 2 - 2 * c * ym)
+    print(Discriminant)
+    return Discriminant
+
+
+def overlay_constraint_circle_circle(c1, c2) -> float:
+    m1 = c1.get_center()
+    m2 = c2.get_center()
+
+    distance = sqrt((m2[0] - m1[0]) ** 2 + (m2[1] - m1[1]) ** 2)
+    print(distance)
+    return distance
+
+
 def overlay_constraint_rectangle_rectangle(rectangle, rectangle2):
     b = False
-    rectangle_blueprint = shapely.geometry.Polygon([(rectangle.get_x(),rectangle.get_y()),
-                                   (rectangle.get_x()+rectangle.get_width(),rectangle.get_y()),
-                                   (rectangle.get_x()+rectangle.get_width(), rectangle.get_y()+rectangle.get_height()),
-                                   (rectangle.get_x(),rectangle.get_y()+rectangle.get_height())
-                                   ])
+    rectangle_blueprint = shapely.geometry.Polygon([(rectangle.get_x(), rectangle.get_y()),
+                                                    (rectangle.get_x() + rectangle.get_width(), rectangle.get_y()),
+                                                    (rectangle.get_x() + rectangle.get_width(),
+                                                     rectangle.get_y() + rectangle.get_height()),
+                                                    (rectangle.get_x(), rectangle.get_y() + rectangle.get_height())
+                                                    ])
 
-    rectangle2_blueprint =shapely.geometry.Polygon([(rectangle2.get_x(),rectangle2.get_y()),
-                                    (rectangle2.get_x()+rectangle2.get_width(), rectangle2.get_y()),
-                                    (rectangle2.get_x()+rectangle2.get_width(), rectangle2.get_y()+rectangle2.get_height()),
-                                    (rectangle2.get_x(),rectangle2.get_y()+rectangle2.get_height())
-                                    ])
+    rectangle2_blueprint = shapely.geometry.Polygon([(rectangle2.get_x(), rectangle2.get_y()),
+                                                     (rectangle2.get_x() + rectangle2.get_width(), rectangle2.get_y()),
+                                                     (rectangle2.get_x() + rectangle2.get_width(),
+                                                      rectangle2.get_y() + rectangle2.get_height()),
+                                                     (rectangle2.get_x(), rectangle2.get_y() + rectangle2.get_height())
+                                                     ])
 
     if rectangle_blueprint.intersects(rectangle2_blueprint):
         print(rectangle_blueprint.intersects(rectangle2_blueprint))
-        b=True
+        b = True
 
     return b
 
-def createRectangle(width_rectangle, height_rectangle):
 
+def createRectangle(width_rectangle, height_rectangle):
     x_rectangle = round(random.uniform(0, 1), 2)
     y_rectangle = round(random.uniform(0, 1), 2)
     if (x_rectangle + width_rectangle) > 1:
@@ -53,59 +73,72 @@ def createRectangle(width_rectangle, height_rectangle):
         y_rectangle = 0 + height_rectangle
 
     angle_rectangle = 0
-    rectangle=pat.Rectangle(xy=(x_rectangle,y_rectangle),width=width_rectangle,height=height_rectangle,angle=angle_rectangle)
+    rectangle = pat.Rectangle(xy=(x_rectangle, y_rectangle), width=width_rectangle, height=height_rectangle,
+                              angle=angle_rectangle)
     return rectangle
 
+
 def createCircle(r_circle=0.1):
-    #Create Randomness
-    x_circle=round(random.uniform(0,1), 2)
-    y_circle=round(random.uniform(0,1), 2)
+    # Create Randomness
+    x_circle = round(random.uniform(0, 1), 2)
+    y_circle = round(random.uniform(0, 1), 2)
 
-    if x_circle+r_circle > 1 :
-        x_circle=1-r_circle
-    if y_circle+r_circle >1:
-        y_circle=1-r_circle
+    if x_circle + r_circle > 1:
+        x_circle = 1 - r_circle
+    if y_circle + r_circle > 1:
+        y_circle = 1 - r_circle
 
-    if x_circle-r_circle <0:
-        x_circle=0+r_circle
-    if y_circle-r_circle <0:
-        y_circle=0+r_circle
+    if x_circle - r_circle < 0:
+        x_circle = 0 + r_circle
+    if y_circle - r_circle < 0:
+        y_circle = 0 + r_circle
 
-    circle=pat.Circle(xy=(x_circle,y_circle),radius=r_circle)
+    circle = pat.Circle(xy=(x_circle, y_circle), radius=r_circle)
     return circle
 
+
 def main():
-    fig,ax = plt.subplots(1)
+    fig, ax = plt.subplots(1)
 
     circle = createCircle(0.1)
-    rectangle = createRectangle(width_rectangle=0.2,height_rectangle=0.2)
-    rectangle2 = createRectangle(width_rectangle=0.2,height_rectangle=0.2)
+    circle2 = createCircle(0.1)
+    rectangle = createRectangle(width_rectangle=0.2, height_rectangle=0.2)
+    rectangle2 = createRectangle(width_rectangle=0.2, height_rectangle=0.2)
 
     b = overlay_constraint_rectangle_rectangle(rectangle, rectangle2)
 
     while b == True:
-        rectangle2= createRectangle(width_rectangle=0.2,height_rectangle=0.2)
+        rectangle2 = createRectangle(width_rectangle=0.2, height_rectangle=0.2)
         b = overlay_constraint_rectangle_rectangle(rectangle, rectangle2)
 
-    triangle=pat.Polygon(xy=[[0,0.3],[0.3,0.3],[0.15,0.4]],closed=True)
+    Discriminant = overlay_constraint_rectangle_circle(rectangle, circle)
+
+    while Discriminant > 0:
+        circle = createCircle(0.1)
+        Discriminant = overlay_constraint_rectangle_circle(rectangle, circle)
+
+    distance = overlay_constraint_circle_circle(circle, circle2)
+
+    while (abs(circle.get_radius() - circle2.get_radius())) < distance < (
+    abs(circle.get_radius() + circle2.get_radius())):
+        circle = createCircle(0.1)
+        distance = overlay_constraint_circle_circle(circle, circle2)
+        print(distance)
+
+    triangle = pat.Polygon(xy=[[0, 0.3], [0.3, 0.3], [0.15, 0.4]], closed=True)
 
     ax.add_patch(circle)
+    ax.add_patch(circle2)
+
     ax.add_patch(rectangle)
     ax.add_patch(rectangle2)
     ax.add_patch(triangle)
 
-    plt.xlim(right=1) #xmax is your value
-    plt.xlim(left=0) #xmin is your value
-    plt.ylim(top=1) #ymax is your value
-    plt.ylim(bottom=0) #ymin is your value
+    plt.xlim(right=1)  # xmax is your value
+    plt.xlim(left=0)  # xmin is your value
+    plt.ylim(top=1)  # ymax is your value
+    plt.ylim(bottom=0)  # ymin is your value
     plt.plot()
-    # Create random x and y data
-    '''
-    N = 500
-    x = np.random.rand(N)
-    y = np.random.rand(N)
-    colors = (0,0,0)
-    area = np.pi*3 '''
 
     plt.title('Scatter plot pythonspot.com')
     plt.xlabel('x')
@@ -114,5 +147,5 @@ def main():
     print("Done")
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     main()
