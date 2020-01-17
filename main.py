8d64199a02644cc789ba8943d41cced861ef671a
gremarsl <marcel.greubel@gmail.com>
2020-01-17 15:03:43 +0100

    -create 3 CircleObjects and plot -comment plotCircle() - create CircleObject with self.attributes

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import random
 from sympy import *
 import numpy as np
 
-class InputObject(object):
 
-    #Room
+class InputObject(object):
+    # Room
     e1 = (0, 0)
     e2 = (0, 500)
     e3 = (350, 500)
@@ -17,7 +17,7 @@ class InputObject(object):
     e7 = (0, 0)
 
     elist = []
-    elist.extend((e1, e2,e3, e4,e5,e6,e7))
+    elist.extend((e1, e2, e3, e4, e5, e6, e7))
 
     i = 0
     room_x_coord = []
@@ -31,7 +31,7 @@ class InputObject(object):
     # Circle
     circle_r = 30
 
-    #Rectangle1
+    # Rectangle1
     rectangle1_width = 100
     rectangle1_height = 100
 
@@ -39,15 +39,47 @@ class InputObject(object):
     rectangle2_width = 10
     rectangle2_height = 50
 
-    #all Rectangles
+    # all Rectangles
     angle_rectangle = 0
+
     def __init__(self):
         pass
+
 
 class Circle(object):
 
-    def __init__(self):
+    '''
+    def plotCircle(self):
+        print("are you running?")
+        print(self.x_circle)
+        print(self.y_circle)
+        print(self.circle_r)
+        fig, ax = plt.subplots(1)
+
+        circle= pat.Circle(xy=(self.x_circle, self.y_circle), radius=self.circle_r)
+        ax.add_patch(circle)
+    '''
+
+    def __init__(self, input, circle_r):
+        self.circle_r = circle_r
+        # Create Randomness
+        xmax = np.max(input.arr)
+        ymax = np.max(input.arr)
+        self.x_circle = round(random.uniform(0, xmax), 2)
+        self.y_circle = round(random.uniform(0, ymax), 2)
+
+        if self.x_circle + input.circle_r > xmax:
+            self.x_circle = xmax - input.circle_r
+        if self.y_circle + input.circle_r > ymax:
+            self.y_circle = ymax - input.circle_r
+
+        if self.x_circle - input.circle_r < 0:
+            self.x_circle = 0 + input.circle_r
+        if self.y_circle - input.circle_r < 0:
+            self.y_circle = 0 + input.circle_r
+
         pass
+
 
 class Rectangle(object):
 
@@ -72,7 +104,7 @@ def overlay_constraint_rectangle_circle(rectangle, circle):
 
     # solution for simplity:
     Discriminant = (-2 * xm + 2 * m * c - 2 * m * ym) ** 2 - 4 * (1 + m ** 2) * (
-                xm ** 2 + c ** 2 + ym ** 2 - r ** 2 - 2 * c * ym)
+            xm ** 2 + c ** 2 + ym ** 2 - r ** 2 - 2 * c * ym)
     print(Discriminant)
     return Discriminant
 
@@ -109,14 +141,14 @@ def overlay_constraint_rectangle_rectangle(rectangle, rectangle2):
     return b
 
 
-def createRectangle(input,width_rectangle, height_rectangle):
-    #TODO constraint to room edges
+def createRectangle(input, width_rectangle, height_rectangle):
+    # TODO constraint to room edges
     xmax = np.max(input.arr)
     ymax = np.max(input.arr)
     x_rectangle = round(random.uniform(0, xmax), 2)
     y_rectangle = round(random.uniform(0, ymax), 2)
     if (x_rectangle + width_rectangle) > xmax:
-        x_rectangle = xmax- width_rectangle
+        x_rectangle = xmax - width_rectangle
     if y_rectangle + height_rectangle > xmax:
         y_rectangle = ymax - height_rectangle
     if (x_rectangle + width_rectangle) < 0:
@@ -124,9 +156,6 @@ def createRectangle(input,width_rectangle, height_rectangle):
     if y_rectangle + height_rectangle < 0:
         y_rectangle = 0 + height_rectangle
 
-    print("examiniation")
-    print(width_rectangle)
-    print(height_rectangle)
     rectangle = pat.Rectangle(xy=(x_rectangle, y_rectangle), width=width_rectangle, height=height_rectangle,
                               angle=input.angle_rectangle)
     return rectangle
@@ -135,7 +164,7 @@ def createRectangle(input,width_rectangle, height_rectangle):
 def createCircle(input):
     # Create Randomness
     xmax = np.max(input.arr)
-    ymax=np.max(input.arr)
+    ymax = np.max(input.arr)
     x_circle = round(random.uniform(0, xmax), 2)
     y_circle = round(random.uniform(0, ymax), 2)
 
@@ -148,15 +177,14 @@ def createCircle(input):
         x_circle = 0 + input.circle_r
     if y_circle - input.circle_r < 0:
         y_circle = 0 + input.circle_r
-
     circle = pat.Circle(xy=(x_circle, y_circle), radius=input.circle_r)
+    # need to exe in main: ax.add_patch(circle)
     return circle
 
 
 def createRoom(input):
-
     elist = []
-    elist.extend((input.e1, input.e2, input.e3, input.e4,input.e5,input.e6,input.e7))
+    elist.extend((input.e1, input.e2, input.e3, input.e4, input.e5, input.e6, input.e7))
     print(elist)
 
     i = 0
@@ -166,18 +194,30 @@ def createRoom(input):
         xplist.append(element[0])
         yplist.append(element[1])
 
-    return xplist,yplist
+    return xplist, yplist
 
 
 def main():
-    inputObject=InputObject()
+    inputObject = InputObject()
 
     fig, ax = plt.subplots(1)
 
     circle = createCircle(inputObject)
-    circle2 = createCircle(inputObject)
-    rectangle = createRectangle(inputObject,width_rectangle=inputObject.rectangle1_width, height_rectangle=inputObject.rectangle1_height)
-    rectangle2 = createRectangle(inputObject,width_rectangle=inputObject.rectangle2_width, height_rectangle=inputObject.rectangle2_height)
+    circleObj2 = Circle(inputObject, inputObject.circle_r)
+    circle2 = pat.Circle(xy=(circleObj2.x_circle, circleObj2.y_circle), radius=circleObj2.circle_r)
+
+    circleObj = Circle(inputObject, inputObject.circle_r)
+    circle = pat.Circle(xy=(circleObj.x_circle, circleObj.y_circle), radius=circleObj.circle_r)
+
+    circleObject = Circle(inputObject, inputObject.circle_r)
+    circle3 = pat.Circle(xy=(circleObject.x_circle, circleObject.y_circle), radius=circleObject.circle_r)
+
+    print("should be 3")
+
+    rectangle = createRectangle(inputObject, width_rectangle=inputObject.rectangle1_width,
+                                height_rectangle=inputObject.rectangle1_height)
+    rectangle2 = createRectangle(inputObject, width_rectangle=inputObject.rectangle2_width,
+                                 height_rectangle=inputObject.rectangle2_height)
 
     b = overlay_constraint_rectangle_rectangle(rectangle, rectangle2)
 
@@ -194,34 +234,33 @@ def main():
     distance = overlay_constraint_circle_circle(circle, circle2)
 
     while (abs(circle.get_radius() - circle2.get_radius())) < distance < (
-    abs(circle.get_radius() + circle2.get_radius())):
+            abs(circle.get_radius() + circle2.get_radius())):
         circle = createCircle(30)
         distance = overlay_constraint_circle_circle(circle, circle2)
         print(distance)
 
-    #TODO how to manage, that when generated new geometry -> every condition is checked
+    # TODO how to manage, that when generated new geometry -> every condition is checked
     triangle = pat.Polygon(xy=[[0, 0.3], [0.3, 0.3], [0.15, 0.4]], closed=True)
-
 
     ax.add_patch(circle)
     ax.add_patch(circle2)
+    ax.add_patch(circle3)
 
     ax.add_patch(rectangle)
     ax.add_patch(rectangle2)
     ax.add_patch(triangle)
 
-    plt.xlim(right=np.max(inputObject.arr)+1)  # xmax is your value
-    plt.xlim(left=np.min(inputObject.arr)-1)  # xmin is your value
-    plt.ylim(top=np.max(inputObject.arr)+1)  # ymax is your value
-    plt.ylim(bottom=np.min(inputObject.arr)-1)  # ymin is your value
-    plt.plot(inputObject.room_x_coord,inputObject.room_y_coord)
+    plt.xlim(right=np.max(inputObject.arr) + 1)  # xmax is your value
+    plt.xlim(left=np.min(inputObject.arr) - 1)  # xmin is your value
+    plt.ylim(top=np.max(inputObject.arr) + 1)  # ymax is your value
+    plt.ylim(bottom=np.min(inputObject.arr) - 1)  # ymin is your value
+    plt.plot(inputObject.room_x_coord, inputObject.room_y_coord)
     plt.plot()
 
     plt.title('Scatter plot pythonspot.com')
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
-    print("Done")
 
 
 if __name__ == "__main__":
