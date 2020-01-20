0693f25abdf981ab479641adfeb08b368ff0a202
gremarsl <marcel.greubel@gmail.com>
2020-01-20 13:41:16 +0100

    sort all objects in array and go through array to check constraints

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 import myinputobject as myinputobject
 import mygeometry as mygeometry
 import myconstraints
@@ -13,34 +12,51 @@ def main():
 
     fig, ax = plt.subplots(1)
 
+    circleArray=[]
+    rectangleArray=[]
+    rectangleObjectArray=[]
+
     circleObj = mygeometry.Circle(inputObject, inputObject.circle_r)
+    circleArray.append(circleObj)
     circle = pat.Circle(xy=(circleObj.x_circle, circleObj.y_circle), radius=circleObj.circle_r)
 
     circleObj2 = mygeometry.Circle(inputObject, inputObject.circle_r)
+    circleArray.append(circleObj2)
     circle2 = pat.Circle(xy=(circleObj2.x_circle, circleObj2.y_circle), radius=circleObj2.circle_r)
 
-    circleObject = mygeometry.Circle(inputObject, inputObject.circle_r)
-    circle3 = pat.Circle(xy=(circleObject.x_circle, circleObject.y_circle), radius=circleObject.circle_r)
+    circleObj3= mygeometry.Circle(inputObject, inputObject.circle_r)
+    circleArray.append(circleObj3)
+    circle3 = pat.Circle(xy=(circleObj3.x_circle, circleObj3.y_circle), radius=circleObj3.circle_r)
 
     rectangleObj = mygeometry.Rectangle(inputObject, inputObject.rectangle1_width, inputObject.rectangle1_height)
-
+    rectangleObjectArray.append(rectangleObj)
     rectangle = pat.Rectangle(xy=(rectangleObj.x_rectangle, rectangleObj.y_rectangle),
                               width=rectangleObj.width_rectangle, height=rectangleObj.height_rectangle,
                               angle=inputObject.angle_rectangle)
+    rectangleArray.append(rectangle)
 
     rectangleObj2 = mygeometry.Rectangle(inputObject, inputObject.rectangle2_width, inputObject.rectangle2_height)
-
+    rectangleObjectArray.append(rectangleObj2)
     rectangle2 = pat.Rectangle(xy=(rectangleObj2.x_rectangle, rectangleObj2.y_rectangle),
                                width=rectangleObj2.width_rectangle, height=rectangleObj2.height_rectangle,
                                angle=inputObject.angle_rectangle)
-
-    myconstraints.overlay_constraint_rectangle_rectangle(rectangle, rectangle2)
-
-    myconstraints.overlay_constraint_rectangle_circle(rectangleObj, circleObj)
-
-    myconstraints.overlay_constraint_circle_circle(circleObj, circleObj2)
+    rectangleArray.append(rectangle2)
 
     # TODO manage very overlay-condition with every geometry
+
+    for i in range(len(circleArray)):
+        for j in range(i + 1, len(circleArray)):
+            myconstraints.overlay_constraint_circle_circle(circleArray[i], circleArray[j])
+
+    for i in range(len(rectangleArray)):
+        for j in range(i + 1, len(rectangleArray)):
+            myconstraints.overlay_constraint_rectangle_rectangle(rectangleArray[i], rectangleArray[j])
+
+    for rectangle in rectangleObjectArray:
+        for circle in circleArray:
+            myconstraints.overlay_constraint_rectangle_circle(rectangle,circle)
+
+    myconstraints.overlay_constraint_rectangle_circle(rectangleObj, circleObj)
 
     #triangle = pat.Polygon(xy=[[0, 0.3], [0.3, 0.3], [0.15, 0.4]], closed=True)
 
