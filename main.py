df5f993eae2d9186422a02bc6d6cd6126690ecc9
gremarsl <marcel.greubel@gmail.com>
2020-01-21 13:26:54 +0100

    append all  geometry objects to furnitureObjects

--- main.py
+++ main.py
@@ -1,58 +1,56 @@
-import myinputobject as myinputobject
-import mygeometry as mygeometry
+import myinputobject
+import mygeometry
 import myconstraints
-import matplotlib.patches as pat
 import matplotlib.pyplot as plt
 import numpy as np
 from sympy import *
-
 
 def main():
     inputObject = myinputobject.InputObject()
 
     fig, ax = plt.subplots(1)
 
-    circleObjectArray=[]
-    rectangleArray=[]
-    circleArray=[]
-    rectangleObjectArray=[]
+    furnitureobjects=mygeometry.FurnituresObjects()
+
+    #Make the variable a function attribute
 
     circleObj = mygeometry.Circle(inputObject, inputObject.circle_r)
-    circleObjectArray.append(circleObj)
-    circleArray.append(circleObj.patplot())
-
+    furnitureobjects.circleObjectArray.append(circleObj)
+    furnitureobjects.circleArray.append(circleObj.patplot())
     circleObj2 = mygeometry.Circle(inputObject, inputObject.circle_r)
-    circleObjectArray.append(circleObj2)
-    circleArray.append(circleObj2.patplot())
+    furnitureobjects.circleObjectArray.append(circleObj2)
+    furnitureobjects.circleArray.append(circleObj2.patplot())
 
     rectangleObj = mygeometry.Rectangle(inputObject, inputObject.rectangle1_width, inputObject.rectangle1_height)
-    rectangleObjectArray.append(rectangleObj)
-    rectangleArray.append(rectangleObj.patplot())
+    furnitureobjects.rectangleObjectArray.append(rectangleObj)
+    furnitureobjects.rectangleArray.append(rectangleObj.patplot())
 
     rectangleObj2 = mygeometry.Rectangle(inputObject, inputObject.rectangle2_width, inputObject.rectangle2_height)
-    rectangleObjectArray.append(rectangleObj2)
-    rectangleArray.append(rectangleObj2.patplot())
+    furnitureobjects.rectangleObjectArray.append(rectangleObj2)
+    furnitureobjects.rectangleArray.append(rectangleObj2.patplot())
 
 
     #TODO what happens in the array? if object is deleted
-    for i in range(len(circleObjectArray)):
-        for j in range(i + 1, len(circleObjectArray)):
-            myconstraints.overlay_constraint_circle_circle(circleObjectArray[i], circleObjectArray[j])
+    for i in range(len(furnitureobjects.circleObjectArray)):
+        for j in range(i + 1, len(furnitureobjects.circleObjectArray)):
+            print(furnitureobjects.circleObjectArray)
+            myconstraints.overlay_constraint_circle_circle(furnitureobjects.circleObjectArray[i], furnitureobjects.circleObjectArray[j])
+            print(furnitureobjects.circleObjectArray)
 
-    for i in range(len(rectangleArray)):
-        for j in range(i + 1, len(rectangleArray)):
-            myconstraints.overlay_constraint_rectangle_rectangle(rectangleArray[i], rectangleArray[j])
+    for i in range(len(furnitureobjects.rectangleArray)):
+        for j in range(i + 1, len(furnitureobjects.rectangleArray)):
+            myconstraints.overlay_constraint_rectangle_rectangle(furnitureobjects.rectangleArray[i], furnitureobjects.rectangleArray[j])
 
-    for rectangle in rectangleObjectArray:
-        for circle in circleObjectArray:
+    for rectangle in furnitureobjects.rectangleObjectArray:
+        for circle in furnitureobjects.circleObjectArray:
             myconstraints.overlay_constraint_rectangle_circle(rectangle,circle)
 
     #triangle = pat.Polygon(xy=[[0, 0.3], [0.3, 0.3], [0.15, 0.4]], closed=True)
 
-    for element in circleArray:
+    for element in furnitureobjects.circleArray:
         ax.add_patch(element)
 
-    for element in rectangleArray:
+    for element in furnitureobjects.rectangleArray:
         ax.add_patch(element)
 
     plt.xlim(right=np.max(inputObject.arr) + 1)  # xmax is your value
@@ -70,3 +68,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
